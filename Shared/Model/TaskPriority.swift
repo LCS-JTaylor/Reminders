ae2163e3570c355b124a90e3e7eca8a5df26967a
LCS-JTaylor <jtaylor@lcs.on.ca>
2022-01-20 11:50:31 -0500

    Defined data model for our app

---
+++ Shared/Model/TaskPriority.swift
@@ -0,0 +1,14 @@
+//
+//  Taskpriority.swift
+//  Reminders (iOS)
+//
+//  Created by Jackson Taylor on 2022-01-20.
+//
+
+import Foundation
+
+enum TaskPriority {
+    case high
+    case medium
+    case low
+}
