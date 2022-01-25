5c65af642da83f0b0d51ef6f7f8668ec90121cd9
LCS-JTaylor <jtaylor@lcs.on.ca>
2022-01-25 09:38:44 -0500

    added app title

--- Shared/RemindersApp.swift
+++ Shared/RemindersApp.swift
@@ -14,6 +14,9 @@ struct RemindersApp: App {
     
     var body: some Scene {
         WindowGroup {
+            NavigationView {
+                ContentView(store: store)
+            }
             ContentView(store: store)
         }
     }
